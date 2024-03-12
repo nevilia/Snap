40dbf5b2810fdbf59c7e5a6c7e0a3cb6e378da43
nevilia <winter04nevilia@gmail.com>
2024-03-12 22:30:44 +0530

    Created Post Card, logic for CRUD of posts

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -9,19 +9,22 @@ import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
 import { useNavigate } from "react-router-dom"
 
 type PostFormProps = {
-    post?: Models.Document
+    post?: Models.Document,
+    action: 'Create' | 'Update'
 }
 
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost()
+    const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost()
+
     const { user } = useUserContext()
-    const {toast} = useToast()
+    const { toast } = useToast()
     const navigate = useNavigate()
 
     const form = useForm<z.infer<typeof PostValidation>>({
@@ -34,18 +37,35 @@ const PostForm = ({ post }: PostFormProps) => {
         },
     })
 
-    async function onSubmit(value: z.infer<typeof PostValidation>) {
-       const newPost = await createPost({
-        ...value,
-        userId: user.id
-       })
+    async function onSubmit(values: z.infer<typeof PostValidation>) {
+        if(post && action === 'Update') {
+            const updatedPost = await updatePost({
+                ...values,
+                postId: post.$id,
+                imageId:post?.imageId,
+                imageUrl: post?.imageUrl
+            })
 
-       if(!newPost){
-        toast({
-            title: 'Please try again'
+            if(!updatedPost){
+                toast({
+                    title: 'Please try again'
+                })
+            }
+
+            return navigate(`/posts/${post.$id}`)
+        }
+        
+        const newPost = await createPost({
+            ...values,
+            userId: user.id
         })
-       }
-       navigate('/')
+
+        if (!newPost) {
+            toast({
+                title: 'Please try again'
+            })
+        }
+        navigate('/')
     }
 
     return (
@@ -105,7 +125,7 @@ const PostForm = ({ post }: PostFormProps) => {
                 />
                 <div className="flex gap-4 items-center justify-end">
                     <Button type="button" className="shad-button_dark-4">Cancel</Button>
-                    <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+                    <Button type="submit" className="shad-button_primary whitespace-nowrap" disabled={isLoadingCreate || isLoadingUpdate}>{isLoadingCreate || isLoadingUpdate && 'Loading...'} {action} Post</Button>
                 </div>
             </form>
         </Form>
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -9,7 +9,7 @@ type FileUploaderProps = {
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
     const [file, setFile] = useState<File[]>([])
-    const [fileUrl, setFileUrl] = useState('')
+    const [fileUrl, setFileUrl] = useState(mediaUrl)
 
     const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
         setFile(acceptedFiles)
@@ -20,7 +20,7 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
     const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: {
         'image/*': ['.png', '.jpeg', '.jpg', '.svg']
     } })
-    
+
     return (
         <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'>
             <input {...getInputProps()} className='cursor-pointer' />
